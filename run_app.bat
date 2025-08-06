8cc4a3b795a9b3949e80f1a23053a92c07fc31a0
NipunKeshan <nipunbis2002@gmail.com>
2025-08-06 12:37:15 +0530

    Enchansed Project

---
+++ run_app.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Starting Loan Default Risk Prediction App...
+echo.
+echo Make sure you have run the Jupyter notebook (main.ipynb) first to train the model.
+echo.
+pause
+streamlit run streamlit_app.py
